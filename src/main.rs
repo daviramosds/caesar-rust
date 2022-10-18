ee76e5450dae980f42f2083888b05661f14b8834
daviramosds <davirds.dev@gmail.com>
2022-10-18 06:46:29 -0300

    update: enc function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 fn enc(text: &str, key: u8) -> String {
+    let upper_text = text.to_uppercase();
+    upper_text
+        .chars()
+        .map(|c| {
+            let l = b'A';
+            let c_u8 = c as u8;
+
+            return (l + (c_u8 + key - l) % 26) as char
+        })
+        .collect()
 }
 
 fn main() {
-    let text = enc("hello", 3);
+    let text = enc("", 3);
     println!("{text}")
 }
